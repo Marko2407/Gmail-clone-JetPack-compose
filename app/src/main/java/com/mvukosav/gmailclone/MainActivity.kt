e44b019ba75660df2146f190755d86383bd89cc0
marko.vukosav <marko.vukosav@rimac-automobili.com>
2023-03-05 16:45:08 +0100

    add home app bar component and implement on click listener for opening drawer

---
+++ app/src/main/java/com/mvukosav/gmailclone/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.mvukosav.gmailclone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mvukosav.gmailclone.components.HomeAppBar
+import com.mvukosav.gmailclone.ui.theme.GmailCloneTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GmailCloneTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    GmailApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GmailApp() {
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = { HomeAppBar(scaffoldState, scope) },
+        drawerContent = {
+
+        }
+
+    ) {
+        it.calculateBottomPadding()
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    GmailCloneTheme {
+        GmailApp()
+    }
+}
---
+++ app/src/main/java/com/mvukosav/gmailclone/components/HomeAppBar.kt
@@ -0,0 +1,83 @@
+package com.mvukosav.gmailclone.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mvukosav.gmailclone.GmailApp
+import com.mvukosav.gmailclone.R
+import com.mvukosav.gmailclone.ui.theme.GmailCloneTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@Composable
+fun HomeAppBar(scaffoldState: ScaffoldState, scope: CoroutineScope) {
+    Box(modifier = Modifier.padding(10.dp)) {
+        Card(
+            modifier = Modifier.requiredHeight(50.dp),
+            elevation = 6.dp,
+            shape = RoundedCornerShape(10.dp),
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp)
+            ) {
+                IconButton(onClick = {
+                    scope.launch {
+                        scaffoldState.drawerState.open()
+                    }
+                }) {
+                    Icon(
+                        Icons.Default.Menu,
+                        contentDescription = "Menu item"
+                    )
+                }
+
+                Text(
+                    text = "Search", modifier = Modifier
+                        .weight(2.0f)
+                        .padding(start = 10.dp)
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.restaurant),
+                    contentDescription = "Profile",
+                    modifier = Modifier
+                        .size(30.dp)
+                        .clip(CircleShape)
+                        .background(Color.Transparent)
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    GmailCloneTheme {
+        GmailApp()
+    }
+}
